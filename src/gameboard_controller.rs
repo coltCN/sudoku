af5f16f03cbdd5cbe4f3d635b6fd5569e1c64bc3
马锦峰 <majf.colt@gmail.com>
2021-08-02 07:31:47 +0800

    增加鼠标事件

--- src/gameboard_controller.rs
+++ src/gameboard_controller.rs
@@ -7,13 +7,25 @@ use piston::input::GenericEvent;
 pub struct GameboardController {
     /// Stores the gameboard state.
     pub gameboard: Gameboard,
+    /// Selected cell.
+    pub selected_cell: Option<[usize; 2]>,
+    /// Stores last mouse cursor position.
+    cursor_pos: [f64; 2],
 }
 
 impl GameboardController {
     /// Create a new gameboard controller.
     pub fn new(gameboard: Gameboard) -> GameboardController {
-        GameboardController { gameboard }
+        GameboardController {
+            gameboard,
+            selected_cell: None,
+            cursor_pos: [0.0; 2],
+        }
     }
     /// handles events.
-    pub fn event<E: GenericEvent>(&mut self, e: &E) {}
+    pub fn event<E: GenericEvent>(&mut self, e: &E) {
+        if let Some(pos) = e.mouse_cursor_args() {
+            self.cursor_pos = pos;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,17 @@ extern crate piston;
 
 pub use crate::gameboard::Gameboard;
 pub use crate::gameboard_controller::GameboardController;
+pub use crate::gameboard_view::{GameboardView, GameboardViewSettings};
+
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventLoop, EventSettings, Events};
 use piston::input::RenderEvent;
 use piston::window::WindowSettings;
+
 mod gameboard;
 mod gameboard_controller;
+mod gameboard_view;
 
 fn main() {
     let opengl = OpenGL::V3_2;
@@ -25,12 +29,19 @@ fn main() {
     let mut events = Events::new(EventSettings::new().lazy(true));
     let mut gl = GlGraphics::new(opengl);
 
+    let gameboard = Gameboard::new();
+    let mut gameboard_controller = GameboardController::new(gameboard);
+    let gameboard_view_settings = GameboardViewSettings::new();
+    let gameboard_view = GameboardView::new(gameboard_view_settings);
+
     while let Some(e) = events.next(&mut window) {
+        gameboard_controller.event(&e);
         if let Some(args) = e.render_args() {
             gl.draw(args.viewport(), |c, g| {
                 use graphics::clear;
 
                 clear([1.0; 4], g);
+                gameboard_view.draw(&gameboard_controller, &c, g);
             });
         }
     }
